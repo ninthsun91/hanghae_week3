39fd255152f18bac1c6209d06eef936b180a51dd
ninthsun91 <50778955+ninthsun91@users.noreply.github.com>
2022-10-04 00:59:40 +0900

    nest routers

--- app.js
+++ app.js
@@ -3,10 +3,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import commentRouter from "./routes/comments.js";
 import indexRouter from "./routes/index.js";
-import postRouter from "./routes/post.js";
-import userRouter from "./routes/user.js";
+
 
 dotenv.config();
 
@@ -35,8 +33,6 @@ app.use(express.urlencoded({extended:false}));
 
 // routers
 app.use("/", indexRouter);
-app.use("/comments", commentRouter);
-app.use("/posts", postRouter);
 
 // create 404
 app.use((req, res, next)=>{
@@ -44,6 +40,7 @@ app.use((req, res, next)=>{
 
     res.status(404).json({ "message": error.message });
 });
+
 
 // error handler
 
