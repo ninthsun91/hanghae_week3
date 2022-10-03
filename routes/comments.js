58eb31594b1299aa38014e182be026d296170f01
ninthsun91 <50778955+ninthsun91@users.noreply.github.com>
2022-10-04 00:51:47 +0900

    refactor comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,7 @@
 import express from "express";
 import async from "async";
-import CommentSchema from "../schemas/comment.js";
-import PostSchema from "../schemas/post.js"
+import CommentModel from "../schemas/comment.js";
+import PostModel from "../schemas/post.js"
 
 
 const router = express.Router();
@@ -14,26 +14,22 @@ const router = express.Router();
  */
 router.get("/:_postId", async(req, res, next)=>{
     const postId = req.params._postId;
-    
+
     try {
-        const { commentIds } = await PostSchema
+        const { commentIds } = await PostModel
             .findById(postId)
-            .select({"_id": 0, "commentIds": 1});
-        
-        const data = [];
-        for (const commentId of commentIds) {
-            const comment = await CommentSchema
-                .findById(commentId)
-                .select({ 
+            .select({"_id": 0, "commentIds": 1})
+            .populate({
+                path: "commentIds",
+                select: {
                     "commentId": "$_id",
-                    "_id": 0, "user": 1, "content": 1, "createdAt": 1
-                });
-            data.push(comment);
-        }
-        data.reverse()
-        res.json({ data: data });
+                    "_id": 0, "user": 1, "content": 1, "createdAt": 1,
+                },
+                options: {sort: {createdAt: -1}}
+            });
+
+        res.json({ data: commentIds });
     } catch (error) {
-        console.error(error);
         return next(error);
     }
 });
@@ -55,17 +51,15 @@ router.post("/:_postId", async (req, res, next)=>{
     if (comment.content.length === 0) {
         return res.json({ "message": "댓글 내용을 입력해주세요." });
     }
-    console.log(comment)
     
     try {
-        const createComment = await CommentSchema.create(comment);
+        const createComment = await CommentModel.create(comment);
         const commentId = createComment._id;
 
-        await PostSchema.findByIdAndUpdate(postId, {"$push": {"commentIds": commentId}});
+        await PostModel.findByIdAndUpdate(postId, {"$push": {"commentIds": commentId}});
 
         res.json({ "message": "댓글을 생성하였습니다" });
     } catch (err) {
-        console.error(err);
         return next(err);
     }
 });
@@ -78,30 +72,23 @@ router.post("/:_postId", async (req, res, next)=>{
  */
 router.put("/:_commentId", async(req, res, next)=>{
     const commentId = req.params._commentId;
-    const comment = {
-        password: req.body.password,
-        content: req.body.content,
-    };
-    if (comment.content.length === 0) {
+    const { password, content } = req.body;
+
+    if (content.length === 0) {
         return res.json({ "message": "댓글 내용을 입력해주세요." });
     }
 
-    try {
-        const updateComment = await CommentSchema
-            .updateOne({
-                "_id": commentId,
-                "password": {"$eq": comment.password}
-            }, {"$set": comment});
-        
-        if (updateComment.matchedCount === 0) {
-            return res.json({ "message": "비밀번호가 틀렸습니다" });
-        }
+    const updateComment = await CommentModel
+        .updateOne({
+            "_id": commentId,
+            "password": password,
+        }, {"$set": { content }});
 
-        res.json({ "message": "댓글을 수정하였습니다." })
-    } catch (err) {
-        console.error(err);
-        return next(err);
+    if (updateComment.matchedCount === 0) {
+        return res.json({ "message": "비밀번호가 틀렸습니다" });
     }
+
+    res.json({ "message": "댓글을 수정하였습니다." })
 });
     
 
@@ -111,37 +98,31 @@ router.put("/:_commentId", async(req, res, next)=>{
  router.delete("/:_commentId", async(req, res, next)=>{
     const commentId = req.params._commentId;
 
-    try {
-        const comment = await CommentSchema.findOne({
-                    "_id": commentId,
-                    "password": req.body.password,
-                });
-        if (comment === null) {
-            return res.json({ "message": "비밀번호가 틀렸습니다" });
-        }
-
-        const postId = comment.postId;
-        async.parallel(
-            {
-                deleteComment: (callback)=>{
-                    CommentSchema.findByIdAndDelete(commentId).exec(callback);
-                },
-                updatePost: (callback)=>{
-                    PostSchema.findByIdAndUpdate(postId, {"$pull": {"commentIds": commentId}}).exec(callback);
-                },
-            },
-            function (err, results) {
-                if (err) {
-                    return next(err);
-                }
-
-                res.json({ "message": "댓글을 삭제하였습니다." });
-            }
-        );      
-    } catch (err) {
-        console.error(err);
-        return next(err);
+    const comment = await CommentModel.findOne({
+                "_id": commentId,
+                "password": req.body.password,
+            });
+    if (comment === null) {
+        return res.json({ "message": "비밀번호가 틀렸습니다" });
     }
+
+    const postId = comment.postId;
+    async.parallel(
+        {
+            deleteComment: (callback)=>{
+                CommentModel.findByIdAndDelete(commentId).exec(callback);
+            },
+            updatePost: (callback)=>{
+                PostModel.findByIdAndUpdate(postId, {"$pull": {"commentIds": commentId}}).exec(callback);
+            },
+        },
+        function (err, results) {
+            if (err) {
+                return next(err);
+            }
+            res.json({ "message": "댓글을 삭제하였습니다." });
+        }
+    );
 });
 
 
