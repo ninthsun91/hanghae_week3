e677b325985d25ed57752cf4da0050978601f74f
ninthsun91 <50778955+ninthsun91@users.noreply.github.com>
2022-10-04 01:00:03 +0900

    nest routers

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import PostSchema from "../schemas/post.js";
-import CommentSchema from "../schemas/comment.js";
+
+import commentRouter from "./comments.js";
+import postRouter from "./post.js";
+import userRouter from "./user.js";
 
 const router = express.Router();
 
@@ -9,11 +12,16 @@ router.get("/", (req, res, next)=>{
     res.render("index.html")
 });
 
-
 router.get("/test", async(req, res, next)=>{
     const postList = await PostSchema.find({}, {"_id": 1});
 
     res.json({ postList })
 });
+
+
+router.use("/comments", commentRouter);
+router.use("/posts", postRouter);
+router.use("/user", userRouter);
+
 
 export default router;
